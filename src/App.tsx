e37236291b044ebd5eec539b3e092488a40f3d0f
Abhishek Sharma <abhi761685@gmail.com>
2026-07-22 20:21:54 +0530

    chore: configure React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-function App() {
-  return <h1>Klyro</h1>;
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router/router";
+
+function App(){
+  return <RouterProvider router={router} />;
 }
 
 export default App;
